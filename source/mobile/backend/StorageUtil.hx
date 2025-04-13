48e9e0c93bd7e12bdbdf2a1f7395f67aba11443c
HomuHomu833 <egzozu.be1.bas@gmail.com>
2025-04-13 18:07:24 +0300

    fix for multi user

--- source/mobile/backend/StorageUtil.hx
+++ source/mobile/backend/StorageUtil.hx
@@ -54,7 +54,7 @@ class StorageUtil
 	#if android
 	// always force path due to haxe
 	public static function getExternalStorageDirectory():String
-		return '/storage/emulated/0/.PsychEngine/';
+		return '/sdcard/.PsychEngine/';
 
 	public static function requestPermissions():Void
 	{
