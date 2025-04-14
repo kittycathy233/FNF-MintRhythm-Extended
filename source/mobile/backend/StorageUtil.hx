f216ef7f7bfb42318f8d51b71f1f3f91bc72cabf
HomuHomu833 <egzozu.be1.bas@gmail.com>
2025-04-14 14:29:43 +0300

    add mods folder in .PsychEngine creation
    closes: #304

--- source/mobile/backend/StorageUtil.hx
+++ source/mobile/backend/StorageUtil.hx
@@ -86,8 +86,8 @@ class StorageUtil
 
 		try
 		{
-			if (!FileSystem.exists(StorageUtil.getExternalStorageDirectory()))
-				FileSystem.createDirectory(StorageUtil.getExternalStorageDirectory());
+			if (!FileSystem.exists(StorageUtil.getExternalStorageDirectory() + 'mods'))
+				FileSystem.createDirectory(StorageUtil.getExternalStorageDirectory() + 'mods');
 		}
 		catch (e:Dynamic)
 		{
