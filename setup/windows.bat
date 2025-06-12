ef43e32944f3ae024fb739a09fecc47ef0f38b03
萌之大喵 <146510476+kittycathy233@users.noreply.github.com>
2025-06-12 10:45:18 +0800

    Update windows.bat，是不是你小子让Android打包出错的

--- setup/windows.bat
+++ setup/windows.bat
@@ -3,7 +3,7 @@ color 0a
 cd ..
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install hxcpp 4.3.2 --quiet
+haxelib git hxcpp https://github.com/kittycathy233/hxcpp --quiet
 haxelib git lime https://github.com/kittycathy233/lime --quiet
 haxelib install openfl 9.4.1 --quiet
 haxelib install flixel 6.1.0 --quiet
