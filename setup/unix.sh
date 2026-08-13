63829061482e6d1550c1573f67b6875f9407c34f
kittycathy332 <2286431462@qq.com>
2026-08-13 21:24:45 +0800

    为何要加away3d lib库，万一有人会用到呢

--- setup/unix.sh
+++ setup/unix.sh
@@ -21,6 +21,7 @@ haxelib git flixel-text-input https://github.com/kittycathy332/flixel-text-input
 haxelib install hscript-iris 1.1.3 --quiet
 haxelib install hscript 2.7.0 --quiet
 haxelib install tjson 1.4.0 --quiet
+haxelib install away3d 5.0.9 --quiet
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
 haxelib git linc_luajit https://github.com/kittycathy233/linc_luajit --quiet
 haxelib install hxdiscord_rpc --quiet --skip-dependencies
