38519151c8bd27684d5ab639078c875873a955d6
kittycathy332 <2286431462@qq.com>
2026-08-19 17:09:16 +0800

    之后部分UI计划改用Haxe-UI，因为我觉得它要比现Psych的好看）

--- setup/windows.bat
+++ setup/windows.bat
@@ -29,6 +29,8 @@ haxelib install flxsvg 1.1.0 --quiet
 haxelib install sl-windows-api 1.1.0 --quiet --skip-dependencies
 haxelib install hxWindowColorMode 0.2.0 --quiet --skip-dependencies
 haxelib install extension-androidtools 2.2.0 --quiet --skip-dependencies
+haxelib git haxeui-core https://github.com/kittycathy332/haxeui-core.git --quiet
+haxelib git haxeui-flixel https://github.com/kittycathy332/haxeui-flixel.git master --quiet --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
 haxelib git spine-haxe https://github.com/kittycathy332/spine-haxe-Archive.git  --quiet --skip-dependencies
